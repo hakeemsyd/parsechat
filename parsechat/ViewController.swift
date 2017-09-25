228a857b8aaa6d512fd5f0e0c8a0f79c86fada5e
Syed Hakeem Abbas <hakeemsyd@fb.com>
2017-09-25 08:37:00 -0700

    Parsechat first commit with project

---
+++ parsechat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parsechat
+//
+//  Created by Syed Hakeem Abbas on 9/25/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
